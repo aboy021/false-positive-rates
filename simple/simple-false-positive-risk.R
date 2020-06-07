ee348cbab7c3fe4109124d9d23688b15c463361a
Arthur <arthur.boyer@gmail.com>
2020-06-08 06:31:01 +1200

    Reverted simple to early version including fisher exact. Improved output

--- simple/simple-false-positive-risk.R
+++ simple/simple-false-positive-risk.R
@@ -1,6 +1,6 @@
 rm(list = ls()) # clear all vars from the current workspace
 
-do_test <- function(placebo=0.5, treatment=0.5){
+do_test <- function(placebo=0.5, treatment=0.5, test=chisq.test){
     test_1 <- rbinom(1,100,placebo)
     test_1_minus100 <- 100 - test_1
     dead_alive_1 <- c(test_1, test_1_minus100)
@@ -8,26 +8,32 @@ do_test <- function(placebo=0.5, treatment=0.5){
     test_2_minus100 <- 100 - test_2
     dead_alive_2 <- c(test_2, test_2_minus100)
     matrix_dead_alive <- rbind(dead_alive_1,dead_alive_2)
-    cs <- chisq.test(matrix_dead_alive)
+    cs <- test(matrix_dead_alive)
     return(cs$p.value)
 }
 
 is_significant <- function(p_value){
-    return(p_value >= 0.03 && p_value <= 0.05)
+    return(p_value <= 0.05)
 }
+
+
 
 placebo_prob = 0.5
 treatment_prob = 0.6
+test = fisher.test ## chisq.test
 
-placebo_vs_placebo <- replicate(100000, do_test(placebo_prob,placebo_prob))
-treatment_vs_placebo <- replicate(100000, do_test(placebo_prob,treatment_prob))
+
+placebo_vs_placebo <- replicate(100000, do_test(placebo_prob,placebo_prob, test))
+treatment_vs_placebo <- replicate(100000, do_test(placebo_prob,treatment_prob, test))
 
 false_positives = length(Filter(is_significant, placebo_vs_placebo))
 true_positives = length(Filter(is_significant, treatment_vs_placebo))
+false_positive_risk = false_positives / (true_positives + false_positives)
 
-cat("False Positives: ", false_positives, "\n",
-    "True Positives: ", true_positives, "\n",
-    "False Positive Risk: ", false_positives / (true_positives + false_positives), "\n")
+cat("False Positives", "\t", "True Positives", "\t", "False Positive Risk\n", 
+    false_positives, "\t", true_positives, "\t", false_positive_risk)
 
-> hist(placebo_vs_placebo)
-> hist(treatment_vs_placebo)
+subtitle <- ifelse(identical(test, fisher.test), "Fisher's Exact", "Chi-squared")
+
+hist(placebo_vs_placebo, sub=subtitle)
+hist(treatment_vs_placebo, sub=subtitle)
