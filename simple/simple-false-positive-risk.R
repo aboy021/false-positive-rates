d78c25b9975be2d18217af4eaea02c7e5fc61d64
Arthur <arthur.boyer@gmail.com>
2020-06-12 05:11:57 +1200

    Simple: altered output formatting. Added comments and renamed variables for readability

--- simple/simple-false-positive-risk.R
+++ simple/simple-false-positive-risk.R
@@ -3,17 +3,38 @@ rm(list = ls()) # clear all vars from the current workspace
 
 #########################
 # Simulation Parameters #
+#
+# | prob_hyp_true | prob_hyp_true_factor |
+# | --------------|----------------------|
+# | 0.1           | 9                    |
+# | 0.2           | 4                    |
+# | 0.3           | 7/3                  |
+# | 0.4           | 3/2                  |
+# | 0.5           | 1                    |
+# | 0.6           | 2/3                  |
+# | 0.7           | 3/7                  |
+# | 0.8           | 1/4                  |
+# | 0.9           | 1/9                  |
+#
 #########################
+
+# Trial size and probabilities
 control_prob = 0.5
 treatment_prob = 0.6
 num_subjects = 100
-num_simulations_control_vs_control = 10000
-num_simulations_control_vs_treatment = 10000
+
+# Number of simulations and probability the hypothesis was true
+base_num_simulations = 10000
+prob_hyp_true_factor = 1
+num_simulations_control_vs_treatment = base_num_simulations
+num_simulations_control_vs_control = base_num_simulations * prob_hyp_true_factor
+
+# Tested p value significant range
 min_p_value = 0.0
 max_p_value = 0.05
 
-# test = fisher.test
-test = chisq.test
+test = fisher.test
+#test = chisq.test
 
 
 ###########################
@@ -24,13 +45,15 @@ is_significant <- function(p_value){
 }
 
 do_test <- function(control=0.5, treatment=0.5, test=chisq.test, num_subjects=100){
-    test_1 <- rbinom(1,num_subjects,control)
-    test_1_minus100 <- num_subjects - test_1
-    dead_alive_1 <- c(test_1, test_1_minus100)
-    test_2 <- rbinom(1,num_subjects,treatment)
-    test_2_minus100 <- num_subjects - test_2
-    dead_alive_2 <- c(test_2, test_2_minus100)
-    matrix_dead_alive <- rbind(dead_alive_1,dead_alive_2)
+    control_alive <- rbinom(1,num_subjects,control)
+    control_dead <- num_subjects - control_alive
+    control_dead_alive <- c(control_alive, control_dead)
+    
+    treatment_alive <- rbinom(1,num_subjects,treatment)
+    treatment_dead <- num_subjects - treatment_alive
+    treatment_dead_alive <- c(treatment_alive, treatment_dead)
+    
+    matrix_dead_alive <- rbind(control_dead_alive,treatment_dead_alive)
     cs <- test(matrix_dead_alive)
     return(cs$p.value)
 }
@@ -76,28 +99,17 @@ false_negative_risk = false_negatives / (true_negatives + false_negatives)
 # Display Results #
 ###################
 cat("\n",
-"true_positives ", "\t",
-"false_positives", "\t",
-"true_negatives ", "\t",
-"false_negatives", "\t",
-"total_number_of_trials", "\t",
-"prob_positive_trial", "\t",
-"prob_negative_trial", "\t",
-"positive_predictive_value", "\t",
-"false_positive_risk", "\t",
-"negative_predictive_value", "\t",
-"false_negative_risk", "\n",
-true_positives , "\t",
-false_positives, "\t",
-true_negatives , "\t",
-false_negatives, "\t",
-total_number_of_trials, "\t",
-prob_positive_trial, "\t",
-prob_negative_trial, "\t",
-positive_predictive_value, "\t",
-false_positive_risk, "\t",
-negative_predictive_value, "\t",
-false_negative_risk, "\n\n")
+"total_number_of_trials:", "\t", total_number_of_trials, "\n",
+"true_positives: ", "\t", true_positives, "\n",
+"false_positives:", "\t", false_positives, "\n",
+"true_negatives: ", "\t", true_negatives, "\n",
+"false_negatives:", "\t", false_negatives, "\n",
+"prob_positive_trial:", "\t", prob_positive_trial, "\n",
+"prob_negative_trial:", "\t", prob_negative_trial, "\n",
+"positive_predictive_value:", "\t", positive_predictive_value, "\n",
+"false_positive_risk:", "\t", false_positive_risk, "\n",
+"negative_predictive_value:", "\t", negative_predictive_value, "\n",
+"false_negative_risk:", "\t", false_negative_risk, "\n\n")
 
 
 ############
